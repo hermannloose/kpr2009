8733775655208ac727c28a652f96192dc7a61d33
hermannloose <hermannloose@gmail.com>
2010-02-02 14:38:19 +0100

    Initial commit of modified files (might be broken).

--- l4/pkg/kbd/include/kbd.h
+++ l4/pkg/kbd/include/kbd.h
@@ -1,19 +1,18 @@
 #ifndef KBD_SERVER
 #define KBD_SERVER
 
-#include <l4/cxx/ipc_server>
 #include <l4/cxx/iostream.h>
 #include <l4/sys/irq>
 #include <l4/sys/types.h>
 
-class Kbd_server : public L4::Server_object
+class Kbd_server
 {
   private:
 	  L4::Cap<L4::Icu> icucap;
 	  L4::Cap<L4::Irq> irqcap;
   public:
 	  Kbd_server();
-		int dispatch(l4_umword_t obj, L4::Ipc_iostream &ios);
+		void process();
 };
 
 #endif
--- l4/pkg/kbd/server/src/main.cc
+++ l4/pkg/kbd/server/src/main.cc
@@ -3,29 +3,80 @@
 #include <l4/cxx/iostream.h>
 #include <l4/re/env>
 #include <l4/re/util/cap_alloc>
+#include <l4/re/util/object_registry>
 #include <l4/sys/irq>
 #include <l4/sys/types.h>
 
 #include <iostream>
 #include <stdlib.h>
 
+static L4Re::Util::Object_registry registry(
+	L4Re::Env::env()->main_thread(),
+	L4Re::Env::env()->factory()
+);
+
 Kbd_server::Kbd_server()
 {
-  icucap = L4Re::Util::cap_alloc.alloc<L4::Icu>();
-	if (!icucap.is_valid()) {
-	  std::cerr << "Could not get a valid ICU capability slot!" << std::endl;
+  l4_msgtag_t tag;
+
+	// Create and fill ICU cap.
+	icucap = L4Re::Util::cap_alloc.alloc<L4::Icu>();
+	if (!icucap.is_valid())
+	{
+		std::cerr << "Could not get a valid ICU capability slot!" << std::endl;
 		exit(1);
 	}
-	if (L4Re::Env::env()->names()->query("icu", icucap)) {
-	  std::cerr << "Could not get an ICU capability!" << std::endl;
+	if (L4Re::Env::env()->names()->query("icu", icucap))
+	{
+		std::cerr << "Could not get an ICU capability!" << std::endl;
 		exit(1);
 	}
+
+	// Create and fill IRQ cap.
+	irqcap = L4Re::Util::cap_alloc.alloc<L4::Irq>();
+	if (!irqcap.is_valid())
+	{
+		std::cerr << "Could not get a valid IRQ capability slot!" << std::endl;
+		exit(1);
+	}
+	tag = L4Re::Env::env()->factory()->create_irq(irqcap);
+	if (l4_error(tag))
+	{
+		std::cerr << "Could not create IRQ object!" << std::endl;
+		exit(1);
+	}
+
+  // Bind IRQ to ICU.
+	tag = icucap->bind(0x1, irqcap);
+	if (l4_error(tag))
+	{
+		std::cerr << "Binding IRQ cap to IRQ #1 failed!" << std::endl;
+		exit(1);
+	}
+
+	// Attach to IRQ.
+	tag = irqcap->attach(0xBEEF, 0, L4Re::Env::env()->main_thread());
+	if (l4_error(tag))
+	{
+	  std::cerr << "Could not attach to IRQ!" << std::endl;
+		exit(1);
+	}
+
+	// Publish IRQ.
+	registry.register_obj(irqcap);
+	L4Re::Env::env()->names()->register_obj("irq", irqcap);
 }
 
-int Kbd_server::dispatch(l4_umword_t obj, L4::Ipc_iostream &ios)
+void Kbd_server::process()
 {
+	while (true)
+	{
+  	irqcap->receive(L4_IPC_NEVER); 
+	}
 }
 
 int main(int argc, char **argv)
 {
+	Kbd_server *srv = new Kbd_server();
+	srv->process();
 }
