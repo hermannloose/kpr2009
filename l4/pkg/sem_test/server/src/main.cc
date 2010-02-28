2b259bcbfe1e26e856e572f0643b079c7b88e6d2
hermannloose <hermannloose@gmail.com>
2010-02-28 16:13:57 +0100

    Proves producer/consumer setup to work.

--- l4/pkg/sem_test/server/src/main.cc
+++ l4/pkg/sem_test/server/src/main.cc
@@ -13,26 +13,22 @@
 //static L4::Semaphore *sem;
 sem_t sem;
 
-void *run(void *arg)
+void *produce(void *arg)
+{
+	int iteration = 1;
+	while (1) {
+		printf("Produce: %i\n", iteration++);
+		sem_post(&sem);
+	}
+	return NULL;
+}
+
+void *consume(void *arg)
 {
 	int iteration = 0;
-	while(1)
-	{
-		printf("%x / %i: pre-down\n", pthread_self(), iteration);
-		//enter_kdebug("pre-down");
-		//sem->down();
+	while (1) {
 		sem_wait(&sem);
-		//enter_kdebug("post-down");
-		printf("%x / %i: critical\n", pthread_self(), iteration);
-		//enter_kdebug("pre-up");
-		//sem->up();
-		sem_post(&sem);
-		//enter_kdebug("post-up");
-		printf("%x / %i: post-up\n", pthread_self(), iteration);
-		iteration++;
-		/*l4_timeout_t timeout;
-		timeout.p.rcv.t = rand() % 2000;
-		l4_ipc_sleep(timeout);*/
+		printf("Consume: %i\n", iteration++);
 	}
 	return NULL;
 }
@@ -45,11 +41,10 @@ int main(int argc, char **argv)
 	}
 	sem = new L4::Semaphore(ksem);
 	sem->init(1);*/
-	sem_init(&sem, 0, 1);
+	sem_init(&sem, 0, 0);
 	pthread_t t1, t2;
-	pthread_create(&t1, NULL, &run, NULL);
-	pthread_create(&t2, NULL, &run, NULL);
-	while (1);
+	pthread_create(&t1, NULL, &produce, NULL);
+	pthread_create(&t2, NULL, &consume, NULL);
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
 }
