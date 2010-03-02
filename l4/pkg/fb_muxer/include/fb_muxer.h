db7df2337f6be5c4be871a757ebdc7179840bb5b
hermannloose <hermannloose@gmail.com>
2010-03-02 11:24:25 +0100

    Started dispatch code for Service_::Open in fb_muxer.

--- l4/pkg/fb_muxer/include/fb_muxer.h
+++ l4/pkg/fb_muxer/include/fb_muxer.h
@@ -1,20 +1,36 @@
 #ifndef FB_MUXER
 #define FB_MUXER
 
+#include <l4/cxx/iostream.h>
 #include <l4/cxx/ipc_server>
 #include <l4/re/fb>
+#include <l4/re/util/framebuffer_svr>
 #include <l4/sys/types.h>
 
+#include <list>
+
 static const int Switch = 0xf00;
+
+class FBMuxer;
+class VFB;
 
 class FBMuxer : public L4::Server_object
 {
 	private:
 		L4::Cap<L4Re::Framebuffer> fb;
+		L4Re::Framebuffer::Info info;
+		std::list<VFB*> *vfbs;
 	public:
 		FBMuxer();
-		int dispatch(l4_umword_t obj, L4::Ipc_iostream ios);
+		int dispatch(l4_umword_t obj, L4::Ipc_iostream &ios);
 };
 
+
+class VFB : public L4::Server_object, public L4Re::Util::Framebuffer_svr
+{
+	public:
+		VFB(L4Re::Framebuffer::Info info);
+		int dispatch(l4_umword_t obj, L4::Ipc_iostream &ios);
+};
 
 #endif
--- l4/pkg/fb_muxer/server/src/main.cc
+++ l4/pkg/fb_muxer/server/src/main.cc
@@ -1,11 +1,18 @@
 #include <l4/fb_muxer/fb_muxer.h>
 
-#include <l4/re/util/cap_alloc>
+#include <l4/re/env>
 #include <l4/re/protocols>
 #include <l4/re/service-sys.h>
+#include <l4/re/util/cap_alloc>
+#include <l4/re/util/object_registry>
 
 #include <stdio.h>
 #include <stdlib.h>
+
+static L4Re::Util::Object_registry registry(
+	L4Re::Env::env()->main_thread(),
+	L4Re::Env::env()->factory()
+);
 
 FBMuxer::FBMuxer()
 {
@@ -17,15 +24,20 @@ FBMuxer::FBMuxer()
 		
 		exit(1);
 	}
-	if (l4_error(err = L4Re::Env::env()->names()->query("fb", fb))) {
+	if (L4Re::Env::env()->names()->query("fb", fb)) {
 		printf("Could not get framebuffer capability!\n");
 
 		exit(1);
 	}
+	if (fb->info(&info)) {
+		printf("Could not get framebuffer info!\n");
+	}
+
+	vfbs = new std::list<VFB*>();
 
 }
 
-int FBMuxer::dispatch(l4_umword_t obj, L4::Ipc_iostream ios)
+int FBMuxer::dispatch(l4_umword_t obj, L4::Ipc_iostream &ios)
 {
 	l4_msgtag_t tag;
 	ios >> tag;
@@ -50,7 +62,14 @@ int FBMuxer::dispatch(l4_umword_t obj, L4::Ipc_iostream ios)
 		
 		// Create a new virtual framebuffer.
 		case L4Re::Service_::Open:
+			{
+				VFB *vfb = new VFB(info);
+				vfbs->push_back(vfb);
 
+				registry.register_obj(vfb);
+
+				ios << vfb->obj_cap();
+			}
 			return L4_EOK;
 
 		// Switch to an existing virtual framebuffer for display.
@@ -64,7 +83,31 @@ int FBMuxer::dispatch(l4_umword_t obj, L4::Ipc_iostream ios)
 	}
 }
 
+VFB::VFB(L4Re::Framebuffer::Info info)
+{
+	_info = info;
+}
+
+int VFB::dispatch(l4_umword_t obj, L4::Ipc_iostream &ios)
+{
+	return 0;
+};
+
 int main(int argc, char **argv)
 {
+	FBMuxer *muxer = new FBMuxer();
+
+	registry.register_obj(muxer);
+
+	L4::Server<L4::Basic_registry_dispatcher> server(l4_utcb());
+
+	if (L4Re::Env::env()->names()->register_obj("muxer", muxer->obj_cap())) {
+		printf("Could not register muxer, probably read-only namespace?\n");
+		
+		return 1;
+	}
+
+	server.loop();
+
 	return 0;
 }
