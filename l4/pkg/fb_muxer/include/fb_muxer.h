3372788b9640e50614a2b46347f59f51cd77b8ae
hermannloose <hermannloose@gmail.com>
2010-03-15 11:43:05 +0100

    fb_muxer nearing completion.

--- l4/pkg/fb_muxer/include/fb_muxer.h
+++ l4/pkg/fb_muxer/include/fb_muxer.h
@@ -14,6 +14,7 @@ static const int Switch = 0xf00;
 
 class FBMuxer;
 class VFB;
+class VFB_fb_svr;
 
 class FBMuxer : public L4::Server_object
 {
@@ -34,16 +35,24 @@ class FBMuxer : public L4::Server_object
 };
 
 
-class VFB : public L4::Server_object, public L4Re::Util::Dataspace_svr, public L4Re::Util::Framebuffer_svr
+class VFB : public L4::Server_object, public L4Re::Util::Dataspace_svr
 {
 	private:
 		L4::Cap<L4Re::Dataspace> vfb;
 		l4_addr_t vfb_start;
 	public:
+		VFB_fb_svr *fbsvr;
 		VFB(L4Re::Framebuffer::Info info, int size);
 		~VFB() throw();
 		void buffer();
 		void write_through(l4_addr_t start);
+		int dispatch(l4_umword_t obj, L4::Ipc_iostream &ios);
+};
+
+class VFB_fb_svr : public L4::Server_object, public L4Re::Util::Framebuffer_svr
+{
+	public:
+		VFB_fb_svr(L4Re::Framebuffer::Info, L4::Cap<L4Re::Dataspace>);
 		int dispatch(l4_umword_t obj, L4::Ipc_iostream &ios);
 };
 
--- l4/pkg/fb_muxer/server/src/main.cc
+++ l4/pkg/fb_muxer/server/src/main.cc
@@ -8,6 +8,7 @@
 #include <l4/re/util/object_registry>
 
 #include <assert.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,39 +18,62 @@ static L4Re::Util::Object_registry registry(
 	L4Re::Env::env()->main_thread(),
 	L4Re::Env::env()->factory()
 );
-
+/*
+void unmap_region(void* start, void* end)
+{
+	assert(start < end);
+	void* temp = start;
+	int diff = 0;
+	int unmap_size = 0;
+	while (temp < end) {
+		diff = temp - end;
+		unmap_size = floor(log(diff) / log(2));
+		#if debug
+		printf("Unmapping: 2^%i bytes from %p.\n", unmap_size, temp);
+		#endif
+		l4_task_unmap(L4Re::Env::env()->task().cap(), l4_fpage(temp, unmap_size, L4_FPAGE_RWX), L4_FP_OTHER_SPACES);
+		temp += pow(2, unmap_size);
+	}
+}
+*/
 FBMuxer::FBMuxer()
 {
 	l4_msgtag_t err;
 
 	fb_addr = 0;
 
+	// Get the initial framebuffer.
 	fb = L4Re::Util::cap_alloc.alloc<L4Re::Framebuffer>();
+	
 	if (!fb.is_valid()) {
 		printf("Could not get a valid capability slot!\n");
 		
 		exit(1);
 	}
-
+	
 	if (L4Re::Env::env()->names()->query("fb", fb)) {
 		printf("Could not get framebuffer!\n");
 
 		exit(1);
 	}
 
+	// Get framebuffer info.
 	if (fb->info(&info)) {
 		printf("Could not get framebuffer info!\n");
 
 		exit(1);
 	}
 
+	// Get framebuffer memory.
 	fbds = L4Re::Util::cap_alloc.alloc<L4Re::Dataspace>();
+	
 	if (!fbds.is_valid()) {
 		printf("Could not get a valid dataspace capability for the framebuffer!\n");
 
 		exit(1);
 	}
 
+	// Make framebuffer memory available to the multiplexer.
 	fb->mem(fbds);
 
 	if (L4Re::Env::env()->rm()->attach(&fb_addr, fbds->size(), L4Re::Rm::Search_addr, fbds, 0)) {
@@ -58,12 +82,9 @@ FBMuxer::FBMuxer()
 		exit(1);
 	}
 
-	// FIXME Debug output
+	#if debug
 	printf("real FB @ %p\n", fb_addr);
-
-	if (fb->info(&info)) {
-		printf("Could not get framebuffer info!\n");
-	}
+	#endif
 
 	vfbs = new std::list<VFB*>();
 	selected = 0;
@@ -105,12 +126,10 @@ int FBMuxer::dispatch(l4_umword_t obj, L4::Ipc_iostream &ios)
 				#endif
 				vfbs->push_back(vfb);
 
-				// FIXME: for testing
-				switch_to(vfbs->size() - 1);
-
 				registry.register_obj(vfb);
+				registry.register_obj(vfb->fbsvr);
 
-				ios << vfb->obj_cap();
+				ios << vfb->fbsvr->obj_cap();
 
 			}
 			printf("Created new virtual framebuffer.\n");
@@ -149,8 +168,9 @@ int FBMuxer::switch_to(int which)
 	}
 	selected = which;
 
-	// FIXME Debug output
+	#if debug
 	printf("Got old VFB.\n");
+	#endif
 
 	std::list<VFB*>::iterator switch_to_iter;
 	int to_go = which;
@@ -158,8 +178,9 @@ int FBMuxer::switch_to(int which)
 		if (--to_go < 0) break;
 	}
 
-	// FIXME Debug output
+	#if debug
 	printf("Got new VFB.\n");
+	#endif
 
 	(*selected_iter)->buffer();
 	(*switch_to_iter)->write_through(fb_addr);
@@ -174,6 +195,9 @@ int FBMuxer::switch_to(int which)
 
 VFB::VFB(L4Re::Framebuffer::Info info, int size)
 {
+	#if debug
+	printf("Creating new VFB.\n");
+	#endif
 	vfb_start = 0;
 	
 	vfb = L4Re::Util::cap_alloc.alloc<L4Re::Dataspace>();
@@ -214,9 +238,7 @@ VFB::VFB(L4Re::Framebuffer::Info info, int size)
 	_ds_size = size;
 	_rw_flags = Writable;
 
-	// Set up information for Framebuffer_svr.
-	_fb_ds = vfb;
-	_info = info;
+	fbsvr = new VFB_fb_svr(info, vfb);
 }
 
 VFB::~VFB() throw()
@@ -224,34 +246,42 @@ VFB::~VFB() throw()
 	// FIXME: I don't know whether it's really safe to do *nothing* here?
 }
 
-/*
-Switch back to acting as a virtual framebuffer.
-*/
+// Switch back to acting as a virtual framebuffer.
 void VFB::buffer()
 {
 	l4_addr_t fb_start = _ds_start;
 	_ds_start = vfb_start;
 
+	#if debug
 	printf("VFB %p buffer(): old DS @ %p new DS @ %p\n", this, fb_start, _ds_start);
+	#endif
 
 	// Copy snapshot of real framebuffer into the virtual one.
 	memcpy((void*) _ds_start, (void*) fb_start, vfb->size());
+	
+	#if debug
+	printf("memcpy %p -> %p (%i bytes)\n", fb_start, _ds_start, vfb->size());
+	#endif
 
 	// Unmap all pages in the real framebuffer.
 	l4_addr_t temp = fb_start;
+	
+	#if debug
+	printf("Unmap %p - %p.\n", fb_start, fb_start + vfb->size());
+	#endif
+	
 	while (temp < fb_start + vfb->size()) {
+	
 		#if debug
-		printf("Unmapping page @ %p\n", temp);
+		printf("Unmapping page @ %p size %i\n", temp, l4_fpage_size(l4_fpage(temp, 21, L4_FPAGE_RWX)));
 		#endif
-		// Copied from parthy's code.
-		l4_task_unmap(L4Re::Env::env()->task().cap(), l4_fpage(temp, 10, L4_FPAGE_RWX), L4_FP_OTHER_SPACES);
-		temp += 1024 * 4096;
+
+		l4_task_unmap(L4Re::Env::env()->task().cap(), l4_fpage(temp, 21, L4_FPAGE_RWX), L4_FP_OTHER_SPACES);
+		temp += 1024 * 2048;
 	}
 }
 
-/*
-Forward subsequent memory accesses to the real framebuffer.
-*/
+// Forward subsequent memory accesses to the real framebuffer.
 void VFB::write_through(l4_addr_t start)
 {
 	_ds_start = start;
@@ -262,23 +292,60 @@ void VFB::write_through(l4_addr_t start)
 
 	// Copy snapshot of virtual framebuffer into the real one.
 	memcpy((void*) _ds_start, (void*) vfb_start, vfb->size());
+	
+	#if debug
+	printf("memcpy %p -> %p (%i bytes)\n", vfb_start, _ds_start, vfb->size());
+	#endif
 
 	// Unmap all pages in the virtual framebuffer.
+	//unmap_region(vfb_start, vfb_start + vfb->size());
 	l4_addr_t temp = vfb_start;
+	
+	#if debug
+	printf("Unmap %p - %p.\n", vfb_start, vfb_start + vfb->size());
+	#endif
+
 	while (temp < vfb_start + vfb->size()) {
+		
 		#if debug
-		printf("Unmapping page @ %p\n", temp);
+		printf("Unmapping page @ %p size %i\n", temp, l4_fpage_size(l4_fpage(temp, 21, L4_FPAGE_RWX)));
 		#endif
-		l4_task_unmap(L4Re::Env::env()->task().cap(), l4_fpage(temp, 10, L4_FPAGE_RWX), L4_FP_OTHER_SPACES);
-		temp += 1024 * 4096;
+
+		l4_task_unmap(L4Re::Env::env()->task().cap(), l4_fpage(temp, 21, L4_FPAGE_RWX), L4_FP_OTHER_SPACES);
+		temp += 1024 * 2048;
 	}
 }
 
+// Forward to the default implementation.
 int VFB::dispatch(l4_umword_t obj, L4::Ipc_iostream &ios)
 {
-	return L4Re::Util::Framebuffer_svr::dispatch(obj, ios);
+	#if debug
+	printf("Dataspace_svr dispatching.\n");
+	#endif
+
+	return L4Re::Util::Dataspace_svr::dispatch(obj, ios);
 };
 
+VFB_fb_svr::VFB_fb_svr(L4Re::Framebuffer::Info info, L4::Cap<L4Re::Dataspace> vfb)
+{
+	#if debug
+	printf("Creating new VFB_fb_svr.\n");
+	#endif
+
+	// Set up information for Framebuffer_svr.
+	_fb_ds = vfb;
+	_info = info;
+}
+
+// Forward to the default implementation.
+int VFB_fb_svr::dispatch(l4_umword_t obj, L4::Ipc_iostream &ios)
+{
+	#if debug
+	printf("Framebuffer_svr dispatching.\n");
+	#endif
+
+	return L4Re::Util::Framebuffer_svr::dispatch(obj, ios);
+}
 
 int main(int argc, char **argv)
 {
